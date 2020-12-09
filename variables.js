61dd6a0c5a77cf5cf3c4bc02a6beb6fe284f36a3
Andrew Carrigan <carrigan223@gmail.com>
2020-12-09 11:02:27 -0800

    Variables example

---
+++ variables.js
@@ -0,0 +1,10 @@
+var name = "Andrew Carrigan";
+
+var age;//assigning an empty var
+age = 32;// assigning an undefined var a value 
+//arrays are zero index
+var friends = ["jeremy", "sid", "abhi"];
+
+console.log(friends.length);//length is a property of the array
+console.log(friends[1]);//the one is accesing the second index of array
+
